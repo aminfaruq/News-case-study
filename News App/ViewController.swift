905101fe6ed2fd3e4518008c1a42eca943970291
Amin Faruq <aminfaruk.fa@gmail.com>
2023-05-28 16:16:50 +0800

    initial commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Amin faruq on 28/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
